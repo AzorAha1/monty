e4e339de28f1fc63abb3b11230fb8eace949d5a9
AzorAha1 <wizfaiz@icloud.com>
2023-09-21 14:25:32 +0100

    added pall function

--- command_op.c
+++ command_op.c
@@ -12,6 +12,7 @@ void getf(stack_t **stack, char **opcode, unsigned int line_number)
 	instruction_t ops[] = {
 		{"push", op_push},
 		{"pop", op_pop},
+		{"pall", op_pall},
 		{NULL, NULL},
 	};
 	checker = 0;
@@ -24,11 +25,12 @@ void getf(stack_t **stack, char **opcode, unsigned int line_number)
 			checker = 1;
 			return;
 		}
-		line_num++;
+	line_num++;
 	}
 	if (checker == 0)
 	{
-		printf("L%d: unknown instruction %s", line_num, opcode[0]);
+		printf("L%d: unknown instruction %s", line_number, opcode[0]);
 	}
+	
 	fprintf(stderr, "Error: Unknown opcode \"%s\" on line %u\n", opcode[0], line_number);
 }
