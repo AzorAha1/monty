e2d862864433b9f671a6c856ac2b4bc33d785219
AzorAha1 <wizfaiz@icloud.com>
2023-09-22 22:19:24 +0100

    betty fix

--- free.c
+++ free.c
@@ -1,4 +1,10 @@
 #include "monty.h"
+/**
+ * freestack - function name
+ * @stack: stack
+ * Description - function to free stack after use
+ * Return: void
+ */
 void freestack(stack_t **stack)
 {
 	stack_t *temp;
