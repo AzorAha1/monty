3687b4797b6d5e8127c35c4f09777d3045f512de
AzorAha1 <wizfaiz@icloud.com>
2023-09-22 17:52:56 +0100

    fixing error message for pop

--- selector.c
+++ selector.c
@@ -56,7 +56,7 @@ void op_pop(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || !stack)
 	{
-		printf("L%d: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	tmp = *stack;
